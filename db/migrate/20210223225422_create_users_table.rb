7f78c772198f3e2bf1c2fe2e71df8ea3a3c2a598
Yuliana Alvarado <yalvarado924@gmail.com>
2021-02-23 18:22:03 -0600

    migrated table data

--- db/migrate/20210223225422_create_users_table.rb
+++ db/migrate/20210223225422_create_users_table.rb
@@ -1,6 +1,7 @@
 class CreateUsersTable < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
       t.string :username
       t.string :password_digest
     end
