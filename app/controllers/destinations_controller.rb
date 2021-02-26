2e4ae9017fb44c874c247f224e3c96d3f528ba11
Yuliana Alvarado <yalvarado924@gmail.com>
2021-02-25 18:37:54 -0600

    first pass at creating controllers & views

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -10,29 +10,43 @@ class DestinationsController < ApplicationController
 
     #new
     get '/destinations/new' do
-        erb :new
+        erb :'destinations/new'
     end
 
     #show
     get '/destinations/:id' do
-        erb :show
+        @destination = Destination.find_by(id: params[:id])
+        erb :'destinations/show'
     end
 
     #post view
     post '/destinations' do
+        destination = Destination.new(params)
+        if !destination.name.empty? && !destination.location.empty? && !destination.things_to_do.empty?
+            destination.save
+            redirect '/destinations'
+        else
+            @error = "Name, Location, Things To Do are required fields. Please try again."
+            erb :'destinations/new'
+        end
     end
 
     #edit
     get '/destinations/:id/edit' do
-        erb :edit
+        @destination = Destination.find_by(id:params[:id])
+        erb :'destinations/edit'
     end
 
     #patches edit
     patch '/destinations/:id' do
+        @destination = Destination.find_by(id:params[:id])
     end
 
     #delete
     delete '/destinations/:id' do
+        destination = Destination.find_by(id:params[:id])
+        destination.destroy
+        redirect '/destinations'
     end
 
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,5 @@
 require './config/environment'
 
+use Rack::MethodOverride
 use DestinationsController
 run ApplicationController
