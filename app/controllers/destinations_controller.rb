e44d3364bad28f3ccd99d139d871f84c094e76f2
Yuliana Alvarado <yalvarado924@gmail.com>
2021-03-01 20:35:20 -0600

    updated routes/views to accomodate authorizations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -4,24 +4,31 @@ class DestinationsController < ApplicationController
 
     #index
     get '/destinations' do
+        require_login
         @destinations = Destination.all
         erb :'destinations/index'
     end
 
     #new
     get '/destinations/new' do
+        require_login
         erb :'destinations/new'
     end
 
     #show
     get '/destinations/:id' do
+        require_login
         @destination = Destination.find_by(id: params[:id])
-        erb :'destinations/show'
+        if @destination
+            erb :'destinations/show'
+        else
+            redirect '/destinations'
+        end
     end
 
     #post view
     post '/destinations' do
-        destination = Destination.new(params)
+        destination = current_user.destinations.build(params)
         if !destination.name.empty? && !destination.location.empty? && !destination.things_to_do.empty?
             destination.save
             redirect '/destinations'
@@ -33,13 +40,23 @@ class DestinationsController < ApplicationController
 
     #edit
     get '/destinations/:id/edit' do
+        require_login
         @destination = Destination.find_by(id:params[:id])
         erb :'destinations/edit'
     end
 
     #patches edit
     patch '/destinations/:id' do
+        #require_login
         @destination = Destination.find_by(id:params[:id])
+        if !params["destination"]["name"].empty? && !params["destination"]["location"].empty? && !params["destination"]["things_to_do"].empty?
+        #@destination.update(name: params[:name], image: params[:image], location: params[:location], things_to_do: params[:things_to_do], ideal_time_to_visit: params[:ideal_time_to_visit], current_user: params[:currency])
+            @destination.update(params["destination"])
+            redirect "/destinations/#{params[:id]}"
+        else
+            @error = "Data Invalid, try again"
+            erb :'/destinations/edit'
+        end
     end
 
     #delete
