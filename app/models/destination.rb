a56756b4f6784b31b78a3cc84c57a5aed7ced076
Yuliana Alvarado <yalvarado924@gmail.com>
2021-03-04 18:51:57 -0600

    added validations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,6 @@
 class Destination < ActiveRecord::Base
     belongs_to :user
+    validates :name, presence:true
+    validates :location, presence:true
+    validates :things_to_do, presence:true
 end
