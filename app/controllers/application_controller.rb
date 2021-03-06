25b5f0d90200be88fa9678086860964deba18358
Yuliana Alvarado <yalvarado924@gmail.com>
2021-03-05 20:08:24 -0600

    removed method that was commented out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,14 +20,9 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
     end
 
-    #def require_login
-     # unless logged_in?
-      #  redirect '/login'
-      #end
-    #end
   end
 
 end
