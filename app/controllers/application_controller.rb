44e67ade16892248c4e0cc8487c06386b2433688
Yuliana Alvarado <yalvarado924@gmail.com>
2021-03-07 17:07:40 -0600

    added not_found method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,17 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
+    
     def logged_in?
       !!session[:user_id]
     end
 
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    not_found do
+      redirect '/'
     end
 
   end
